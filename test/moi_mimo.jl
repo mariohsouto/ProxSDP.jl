674578d68307edb90bd8dff679d3f50f4fe1206b
Mario Henrique Souto <mariohsouto@gmail.com>
2018-08-04 03:07:43 -0300

    making the tests more loose

--- test/moi_mimo.jl
+++ test/moi_mimo.jl
@@ -38,7 +38,7 @@ function moi_mimo(optimizer, seed, n)
     Xsq_s = MOI.get.(optimizer, MOI.VariablePrimal(), Xsq)
 
     for i in 1:n+1, j in 1:n+1
-        @test 1.0001> abs(Xsq_s[i,j]) > 0.9999
+        @test 1.01> abs(Xsq_s[i,j]) > 0.99
     end
 
     mimo_eval(s, H, y, L, Xsq_s)
