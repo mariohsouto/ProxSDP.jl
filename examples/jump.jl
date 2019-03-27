8c76e2fdea3069fcd16c14877f3f74df9855efc0
guilhermebodin <guilherme.b.moraes@gmail.com>
2019-03-27 11:08:21 -0300

    fixing manual, ProxSDP accepts more than 1 PSD variable

--- examples/jump.jl
+++ examples/jump.jl
@@ -3,23 +3,13 @@ using ProxSDP, JuMP
 # Create a JuMP model using ProxSDP as the solver
 model = Model(with_optimizer(ProxSDP.Optimizer, log_verbose=true))
 
-# Create a Positive Semidefinite variable
-# Currently ProxSDP is only able to hold one PSD
-# variable and no other variable
 @variable(model, X[1:2,1:2], PSD)
 
-# but you can define pieces of the one PSD
-# variable to hold other variable as in the
-# Canonical SDP format
 x = X[1,1]
 y = X[2,2]
 
-# There is no limits on linear constraints
-# one can define as many as wanted
 @constraint(model, ub_x, x <= 2)
-
 @constraint(model, ub_y, y <= 30)
-
 @constraint(model, con, 1x + 5y <= 3)
 
 # ProxSDP supports maximization or minimization
