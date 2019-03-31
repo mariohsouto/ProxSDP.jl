a69076db927b1178f301c2bfa6378aa9784e0cdc
mariohsouto <mariohsouto@gmail.com>
2019-03-31 14:06:23 -0300

    adding runtests.jl back

--- src/residuals.jl
+++ src/residuals.jl
@@ -2,13 +2,15 @@
 function compute_residual!(pair::PrimalDual, a::AuxiliaryData, primal_residual::CircularVector{Float64}, dual_residual::CircularVector{Float64}, comb_residual::CircularVector{Float64}, mat::Matrices, p::Params)
     # Compute primal residual
     a.Mty_old .+= .- a.Mty .+ (1.0 / (1.0 + p.primal_step)) .* (pair.x_old .- pair.x)
-    primal_residual[p.iter] = norm(a.Mty_old, 2) / (1.0 + max(p.norm_c, maximum(abs.(a.Mty))))
+    # dual_residual[p.iter] = norm(a.Mx_old, 2) / (1.0 + max(p.norm_rhs, maximum(abs.(a.Mx))))
+    primal_residual[p.iter] = norm(a.Mty_old, 2) / (1.0 + p.norm_c)
     # primal_residual[p.iter] = norm(a.Mty_old, 2) / (1.0 + p.norm_c)
 
 
     # Compute dual residual
     a.Mx_old .+= .- a.Mx .+ (1.0 / (1.0 + p.dual_step)) .* (pair.y_old .- pair.y)
-    dual_residual[p.iter] = norm(a.Mx_old, 2) / (1.0 + max(p.norm_rhs, maximum(abs.(a.Mx))))
+    dual_residual[p.iter] = norm(a.Mx_old, 2) / (1.0 + p.norm_rhs)
+    # dual_residual[p.iter] = norm(a.Mx_old, 2) / (1.0 + max(p.norm_rhs, maximum(abs.(a.Mx))))
     # dual_residual[p.iter] = norm(a.Mx_old, 2) / (1.0 + p.norm_rhs)
 
     # Compute combined residual
---
+++ test/runtests.jl
@@ -0,0 +1,8 @@
+
+# path = joinpath(dirname(@__FILE__), "..", "..")
+# push!(Base.LOAD_PATH, path)
+# datapath = joinpath(dirname(@__FILE__), "data")
+
+using ProxSDP
+
+include("moitest.jl")
