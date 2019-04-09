8030e02d2d463f310bb6309d2fb9947a814ff8a7
mariohsouto <mariohsouto@gmail.com>
2019-04-09 01:29:20 -0300

    changing heuristics criteria

--- src/pdhg.jl
+++ src/pdhg.jl
@@ -152,7 +152,7 @@ function chambolle_pock(affine_sets::AffineSets, conic_sets::ConicSets, opt)::CP
 
         # Adaptive reduce rank [warning: heuristic]
         if opt.reduce_rank
-            if p.rank_update > p.window && comb_residual[k - p.window] >= comb_residual[k]
+            if p.rank_update > p.window #&& comb_residual[k - p.window] >= comb_residual[k]
                 for (idx, sdp) in enumerate(conic_sets.sdpcone)
                     p.target_rank[idx] = min(p.target_rank[idx], sdp.sq_side, max(trunc(Int, p.target_rank[idx] / 2.), p.current_rank[idx] + 3))
                 end
