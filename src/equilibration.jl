dcc86d56dea15bde60002de67437293a8ee7e0db
mariohsouto <mariohsouto@gmail.com>
2019-07-01 18:07:57 -0300

    minor modifications in euilibration

--- src/equilibration.jl
+++ src/equilibration.jl
@@ -5,6 +5,8 @@ function equilibrate!(M, aff, max_iters=100, lb=-10., ub=10.)
 
     α = (aff.n / (aff.m + aff.p)) ^ .25
     β = ((aff.m + aff.p) / aff.n ) ^ .25
+    α2, β2 = α^2, β^2
+
     γ = .1
 
     u, v           = zeros(aff.m + aff.p), zeros(aff.n)
@@ -27,7 +29,10 @@ function equilibrate!(M, aff, max_iters=100, lb=-10., ub=10.)
     for iter in 1:max_iters
         @timeit "update diag E" E[diagind(E)] .= exp.(u)
         @timeit "update diag D" D[diagind(D)] .= exp.(v)
-        @timeit "M_" M_ .= E * M * D
+        @timeit "M_" begin 
+            mul!(M_, M, D)
+            mul!(M_, E, M_)
+        end
 
         step_size = 2. / (γ * (iter + 1.))
 
@@ -41,8 +46,17 @@ function equilibrate!(M, aff, max_iters=100, lb=-10., ub=10.)
                 row_norms[i] = row_norm2
             end
         end
-        @timeit "u grad " u_grad .= row_norms .- α ^ 2 .+ γ * u
-        @timeit "u proj " u = box_project(u - step_size * u_grad, lb, ub)
+        @timeit "u grad" begin
+            u_grad .= row_norms
+            u_grad .-= α2
+            # Equilibration row error (E)
+            row_error = norm(u_grad, Inf)
+            u_grad += γ * u
+        end
+        @timeit "u proj " begin
+            u -= step_size * u_grad
+            u = box_project(u, lb, ub)
+        end
 
         # v grad estimate
         @timeit "col norms" begin
@@ -54,13 +68,24 @@ function equilibrate!(M, aff, max_iters=100, lb=-10., ub=10.)
                 col_norms[j] = col_norm2
             end
         end
-        @timeit "v grad " v_grad .= col_norms .- β ^ 2 .+ γ * v
-        @timeit "v proj 1" v .= sum(v - step_size * v_grad) / aff.n
-        @timeit "v proj 2" v = box_project(v, 0., ub)
+        @timeit "v grad" begin
+            v_grad .= col_norms
+            v_grad .-= β2
+            # Equilibration column error (D)
+            col_error = norm(u_grad, Inf)
+            v_grad += γ * v
+        end
+        @timeit "v proj" begin
+            v -= step_size * v_grad
+            v .= sum(v) / aff.n
+            v = box_project(v, 0., ub)
+        end
         
         # Update averages.
         @timeit "u update" u_ = 2 * u / (iter + 2) + iter * u_ / (iter + 2)
         @timeit "v update" v_ = 2 * v / (iter + 2) + iter * v_ / (iter + 2)
+
+        # @show max(row_error, col_error)
     end
 
     @timeit "update diag E" E[diagind(E)] .= exp.(u_)
