5a19ee17c75b96ceb26ffe633e7891ca650ef44d
joaquimg <joaquimdgarcia@gmail.com>
2019-07-10 02:01:21 -0300

    reset tests

--- test/runbench.jl
+++ test/runbench.jl
@@ -1,5 +1,5 @@
 path = joinpath(dirname(@__FILE__), "..", "..")
-push!(Base.LOAD_PATH, path)
+# push!(Base.LOAD_PATH, path)
 datapath = joinpath(dirname(@__FILE__), "data")
 
 is_julia1 = VERSION >= v"1.0"
@@ -16,10 +16,11 @@ end
 
 use_MOI = false
 sets_to_test = Symbol[]
+# push!(sets_to_test, :RANDSDP)
+# push!(sets_to_test, :SENSORLOC)
+# push!(sets_to_test, :SDPLIB)
 push!(sets_to_test, :MIMO)
-push!(sets_to_test, :RANDSDP)
-push!(sets_to_test, :SENSORLOC)
-push!(sets_to_test, :SDPLIB)
+
 
 @static if use_MOI
     using ProxSDP, MathOptInterface
@@ -36,7 +37,7 @@ else
     solvers = Tuple{String, Function}[]
     using JuMP
     using ProxSDP
-    push!(solvers, ("ProxSDP", () -> ProxSDP.Optimizer(log_verbose=true, timer_verbose = false, time_limit = 900.0, log_freq = 1_000)))
+    push!(solvers, ("ProxSDP", () -> ProxSDP.Optimizer(log_verbose=true, timer_verbose = true, time_limit = 900.0, log_freq = 1_000)))
     # push!(solvers, ("ProxSDP_fullrank", () -> ProxSDP.Optimizer(full_eig_decomp = true, log_verbose=true, timer_verbose = false, time_limit = 900.0, log_freq = 1_000)))
     # using MosekTools
     # push!(solvers, ("MOSEK", () -> Mosek.Optimizer(MSK_DPAR_OPTIMIZER_MAX_TIME = 900.0))) # eps = ???
@@ -73,24 +74,52 @@ function println2(FILE, solver::String, class::String, ref::String, sol)
 end
 
 RANDSDP_TEST_SET = 1:1
-SENSORLOC_TEST_SET = 50:50:100
-MIMO_TEST_SET = [50, 100]
+SENSORLOC_TEST_SET = [#100:100:300#1000
+    100,
+    200,
+    300,
+    400, # mosek stops here - maybenot in new form
+    500, # proxsdp 166s
+    600,
+    700,
+    800,
+    900,
+    1000,
+]
+MIMO_TEST_SET = [
+    100,
+    500, # mosek stops here
+    1000, # 700s on scs
+    1500, # SCS stops here (?)
+    2000,
+    2500,
+    3000,
+    3500,
+    4000,
+    4500,
+    5000,
+    6000,
+    7000,
+    8000,
+    9000,
+    10_000,
+    ]
 GPP_TEST_SET = [
     "gpp124-1.dat-s",
     "gpp124-1.dat-s",
     "gpp124-2.dat-s",
     "gpp124-3.dat-s",
     "gpp124-4.dat-s",
-    # "gpp250-1.dat-s",
-    # "gpp250-2.dat-s",
-    # "gpp250-3.dat-s",
-    # "gpp250-4.dat-s",
-    # "gpp500-1.dat-s", # SCS > 1000 s
-    # "gpp500-2.dat-s", # SCS > 1000 s
-    # "gpp500-3.dat-s",
-    # "gpp500-4.dat-s",
-    # "equalG11.dat-s",
-    # "equalG51.dat-s",
+    "gpp250-1.dat-s",
+    "gpp250-2.dat-s",
+    "gpp250-3.dat-s",
+    "gpp250-4.dat-s",
+    "gpp500-1.dat-s", # SCS > 1000 s
+    "gpp500-2.dat-s", # SCS > 1000 s
+    "gpp500-3.dat-s",
+    "gpp500-4.dat-s",
+    "equalG11.dat-s",
+    "equalG51.dat-s",
 ]
 MAXCUT_TEST_SET = [
     "mcp124-1.dat-s",
@@ -98,17 +127,17 @@ MAXCUT_TEST_SET = [
     "mcp124-2.dat-s",
     "mcp124-3.dat-s",
     "mcp124-4.dat-s",
-    # "mcp250-1.dat-s",
-    # "mcp250-2.dat-s",
-    # "mcp250-3.dat-s",
-    # "mcp250-4.dat-s",
-    # "mcp500-1.dat-s",
-    # "mcp500-2.dat-s",
-    # "mcp500-3.dat-s",
-    # "mcp500-4.dat-s",
-    # "maxG11.dat-s"  ,
-    # "maxG51.dat-s"  ,
-    # "maxG32.dat-s"  ,
+    "mcp250-1.dat-s",
+    "mcp250-2.dat-s",
+    "mcp250-3.dat-s",
+    "mcp250-4.dat-s",
+    "mcp500-1.dat-s",
+    "mcp500-2.dat-s",
+    "mcp500-3.dat-s",
+    "mcp500-4.dat-s",
+    "maxG11.dat-s"  ,
+    "maxG51.dat-s"  ,
+    "maxG32.dat-s"  ,
     # "maxG55.dat-s"  ,
     # "maxG60.dat-s"  ,
 ]
@@ -151,15 +180,6 @@ for optimizer in solvers
             println2(FILE, optimizer[1], "RANDSDP", "$i", sol)
         end
     end
-    if :MIMO in sets_to_test
-        println("MIMO")
-        _mimo(optimizer[2], 0, 100)
-        for n in MIMO_TEST_SET
-            @show n
-            sol = _mimo(optimizer[2], 0, n)
-            println2(FILE, optimizer[1], "MIMO", "$n", sol)
-        end
-    end
     if :SENSORLOC in sets_to_test
         println("SENSORLOC")
         for n in SENSORLOC_TEST_SET
@@ -180,6 +200,15 @@ for optimizer in solvers
             println(name)
             sol = _sdplib(optimizer[2], joinpath(datapath, name))
             println2(FILE, optimizer[1], "SDPLIB_mc", name, sol)
+        end
+    end
+    if :MIMO in sets_to_test
+        println("MIMO")
+        _mimo(optimizer[2], 0, 100)
+        for n in MIMO_TEST_SET
+            @show n
+            sol = _mimo(optimizer[2], 0, n)
+            println2(FILE, optimizer[1], "MIMO", "$n", sol)
         end
     end
 end
