1ce44efe58407356c6fb93fd505c2da6a0a0e6f8
joaquimg <joaquimdgarcia@gmail.com>
2019-07-10 02:01:10 -0300

    tune equilibration

--- src/equilibration.jl
+++ src/equilibration.jl
@@ -16,16 +16,26 @@ function equilibrate!(M, aff, max_iters=100, lb=-10., ub=10.)
     D = Diagonal(v)
     M_ = copy(M)
 
-    (I, J, V) = findnz(M)
-    cols4row, rows4col = Dict(i => [] for i in 1:aff.m + aff.p), Dict(j => [] for j in 1:aff.n)
-    for idx in 1:length(V)
-        append!(cols4row[I[idx]], J[idx])
-        append!(rows4col[J[idx]], I[idx])
-    end
+    # (I, J, V) = findnz(M)
+    # cols4row, rows4col = Dict(i => [] for i in 1:aff.m + aff.p), Dict(j => [] for j in 1:aff.n)
+    # for idx in 1:length(V)
+    #     append!(cols4row[I[idx]], J[idx])
+    #     append!(rows4col[J[idx]], I[idx])
+    # end
+
+    rows_M_ = rowvals(M_)
 
     for iter in 1:max_iters
-        @timeit "update diag E" E[diagind(E)] .= exp.(u)
-        @timeit "update diag D" D[diagind(D)] .= exp.(v)
+        # @timeit "update diag E" E[diagind(E)] .= exp.(u)
+        # @timeit "update diag D" D[diagind(D)] .= exp.(v)
+        @timeit "update diags" begin
+            for i in eachindex(u)
+                E[i,i] = exp(u[i])
+            end
+            for j in eachindex(v)
+                D[j,j] = exp(v[j])
+            end
+        end
         @timeit "M_" begin 
             mul!(M_, M, D)
             mul!(M_, E, M_)
@@ -36,55 +46,71 @@ function equilibrate!(M, aff, max_iters=100, lb=-10., ub=10.)
 
         # u gradient step
         @timeit "row norms" begin
-            for (i, cols) in cols4row
-                row_norm2 = 0.
-                for j in cols
-                    row_norm2 += M_[i, j]^2
+            fill!(row_norms, 0.0)
+            # for (i, cols) in cols4row
+            #     row_norm2 = 0.
+            #     for j in cols
+            #         row_norm2 += M_[i, j]^2
+            #     end
+            #     row_norms[i] = row_norm2
+            # end
+            for col in 1:aff.n
+                for line in nzrange(M_, col)
+                    row_norms[rows_M_[line]] += abs2(M_[rows_M_[line], col])
                 end
-                row_norms[i] = row_norm2
             end
         end
         @timeit "u grad" begin
-            u_grad .= row_norms
-            u_grad .-= α2
-            # Equilibration row error (E)
-            row_error = norm(u_grad, Inf)
-            u_grad += γ * u
+            # u_grad .= row_norms
+            # u_grad .-= α2
+            # # Equilibration row error (E)
+            # # row_error = norm(u_grad, Inf)
+            # u_grad .+= γ .* u
+
+            @. u_grad = row_norms - α2 + γ * u
         end
         @timeit "u proj " begin
-            u -= step_size * u_grad
-            u = box_project(u, lb, ub)
+            @. u -= step_size * u_grad
+            box_project!(u, lb, ub)
         end
 
         # v grad estimate
         @timeit "col norms" begin
-            for (j, rows) in rows4col
-                col_norm2 = 0.
-                for i in rows
-                    col_norm2 += M_[i, j]^2
+            fill!(col_norms, 0.0)
+            # for (j, rows) in rows4col
+            #     col_norm2 = 0.
+            #     for i in rows
+            #         col_norm2 += M_[i, j]^2
+            #     end
+            #     col_norms[j] = col_norm2
+            # end
+            for col in 1:aff.n
+                for line in nzrange(M_, col)
+                    col_norms[col] += abs2(M_[rows_M_[line], col])
                 end
-                col_norms[j] = col_norm2
             end
         end
         @timeit "v grad" begin
-            v_grad .= col_norms
-            v_grad .-= β2
-            # Equilibration column error (D)
-            col_error = norm(v_grad, Inf)
-            v_grad += γ * v
+            # v_grad .= col_norms
+            # v_grad .-= β2
+            # # Equilibration column error (D)
+            # # col_error = norm(v_grad, Inf)
+            # v_grad .+= γ .* v
+
+            @. v_grad = col_norms - β2 + γ * v
         end
         @timeit "v proj" begin
-            v -= step_size * v_grad
+            @. v -= step_size * v_grad
             v .= sum(v) / aff.n
-            v = box_project(v, 0., ub)
+            box_project!(v, 0., ub)
         end
         
         # Update averages.
         @timeit "u update" begin
-            u_ .= 2 * u / (iter + 2) + iter * u_ / (iter + 2)
+            @. u_ = 2 * u / (iter + 2) + iter * u_ / (iter + 2)
         end
         @timeit "v update" begin
-            v_ = 2 * v / (iter + 2) + iter * v_ / (iter + 2)
+            @. v_ = 2 * v / (iter + 2) + iter * v_ / (iter + 2)
         end
     end
 
@@ -94,6 +120,7 @@ function equilibrate!(M, aff, max_iters=100, lb=-10., ub=10.)
     return E, D
 end
 
-function box_project(y, lb, ub)
-    return min.(ub, max.(y, lb))
+function box_project!(y, lb, ub)
+    y .= min.(ub, max.(y, lb))
+    nothing
 end
