40d9ddcfc36b573e2a6cdeede31fbf7e428b82cb
joaquimg <joaquimdgarcia@gmail.com>
2019-07-11 01:29:56 -0300

    minor opt in equilibration

--- src/equilibration.jl
+++ src/equilibration.jl
@@ -35,51 +35,41 @@ function equilibrate!(M, aff, opt)
         step_size = 2. / (γ * (iter + 1.))
 
         # u gradient step
-        @timeit "row norms" begin
+        @timeit "norms" begin
             fill!(row_norms, 0.0)
-            for col in 1:aff.n
-                for line in nzrange(M_, col)
-                    row_norms[rows_M_[line]] += abs2(M_[rows_M_[line], col])
-                end
-            end
-        end
-        @timeit "u grad" begin
-            @. u_grad = row_norms - α2 + γ * u
-        end
-        @timeit "u proj " begin
-            @. u -= step_size * u_grad
-            box_project!(u, lb, ub)
-        end
-
-        # v grad estimate
-        @timeit "col norms" begin
             fill!(col_norms, 0.0)
             for col in 1:aff.n
                 for line in nzrange(M_, col)
+                    row_norms[rows_M_[line]] += abs2(M_[rows_M_[line], col])
                     col_norms[col] += abs2(M_[rows_M_[line], col])
                 end
             end
         end
-        @timeit "v grad" begin
+        @timeit "grad" begin
+            @. u_grad = row_norms - α2 + γ * u
             @. v_grad = col_norms - β2 + γ * v
         end
-        @timeit "v proj" begin
+        @timeit "proj " begin
+            # u
+            @. u -= step_size * u_grad
+            box_project!(u, lb, ub)
+            # v
             @. v -= step_size * v_grad
-            v .= sum(v) / aff.n
+            sum_v = sum(v)
+            v .= sum_v / aff.n
             box_project!(v, 0., ub)
         end
-        
         # Update averages.
-        @timeit "u update" begin
+        @timeit "update" begin
             @. u_ = 2 * u / (iter + 2) + iter * u_ / (iter + 2)
-        end
-        @timeit "v update" begin
             @. v_ = 2 * v / (iter + 2) + iter * v_ / (iter + 2)
         end
     end
 
-    @timeit "update diag E" E[diagind(E)] .= exp.(u_)
-    @timeit "update diag D" D[diagind(D)] .= exp.(v_)
+    @timeit "update diags" begin 
+        E.diag .= exp.(u_)
+        D.diag .= exp.(v_)
+    end
 
     return E, D
 end
