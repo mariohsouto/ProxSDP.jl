17eda4d97aae4ab8ceb5b70dbf82113766941728
Mario Souto <mariohsouto@gmail.com>
2023-04-30 19:09:29 -0700

    Adding PrecompileTools.jl

--- src/ProxSDP.jl
+++ src/ProxSDP.jl
@@ -1,30 +1,34 @@
 module ProxSDP
 
-import Arpack
-import KrylovKit
-import MathOptInterface
-import TimerOutputs
-import TimerOutputs: @timeit
+using PrecompileTools
 
-import Printf
-import SparseArrays
-import LinearAlgebra
-import LinearAlgebra: BlasInt
-import Random
-import Logging
+@compile_workload begin
+    import Arpack
+    import KrylovKit
+    import MathOptInterface
+    import TimerOutputs
+    import TimerOutputs: @timeit
+
+    import Printf
+    import SparseArrays
+    import LinearAlgebra
+    import LinearAlgebra: BlasInt
+    import Random
+    import Logging
 
 
-include("structs.jl")
-include("options.jl")
-include("util.jl")
-include("printing.jl")
-include("scaling.jl")
-include("equilibration.jl")
-include("pdhg.jl")
-include("residuals.jl")
-include("eigsolver.jl")
-include("prox_operators.jl")
+    include("structs.jl")
+    include("options.jl")
+    include("util.jl")
+    include("printing.jl")
+    include("scaling.jl")
+    include("equilibration.jl")
+    include("pdhg.jl")
+    include("residuals.jl")
+    include("eigsolver.jl")
+    include("prox_operators.jl")
 
-include("MOI_wrapper.jl")
+    include("MOI_wrapper.jl")
+end
 
 end
