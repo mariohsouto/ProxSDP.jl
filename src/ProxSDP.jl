b02a529eb10ac75f1dc9c4becd487b31447d940b
Mario Souto <mariohsouto@gmail.com>
2023-04-30 20:49:24 -0700

    Including PrecompileTools.jl in module

--- src/ProxSDP.jl
+++ src/ProxSDP.jl
@@ -1,34 +1,40 @@
 module ProxSDP
 
-using PrecompileTools
+using PrecompileTools: @setup_workload, @compile_workload
 
-@compile_workload begin
-    import Arpack
-    import KrylovKit
-    import MathOptInterface
-    import TimerOutputs
-    import TimerOutputs: @timeit
+import Arpack
+import KrylovKit
+import MathOptInterface
+import TimerOutputs
+import TimerOutputs: @timeit
 
-    import Printf
-    import SparseArrays
-    import LinearAlgebra
-    import LinearAlgebra: BlasInt
-    import Random
-    import Logging
+import Printf
+import SparseArrays
+import LinearAlgebra
+import LinearAlgebra: BlasInt
+import Random
+import Logging
 
 
-    include("structs.jl")
-    include("options.jl")
-    include("util.jl")
-    include("printing.jl")
-    include("scaling.jl")
-    include("equilibration.jl")
-    include("pdhg.jl")
-    include("residuals.jl")
-    include("eigsolver.jl")
-    include("prox_operators.jl")
+include("structs.jl")
+include("options.jl")
+include("util.jl")
+include("printing.jl")
+include("scaling.jl")
+include("equilibration.jl")
+include("pdhg.jl")
+include("residuals.jl")
+include("eigsolver.jl")
+include("prox_operators.jl")
+include("MOI_wrapper.jl")
 
-    include("MOI_wrapper.jl")
+# PrecompileTools
+@setup_workload begin
+
+    @compile_workload begin
+        include("../test/run_mini_benchmark.jl")
+    end
+
 end
 
 end
